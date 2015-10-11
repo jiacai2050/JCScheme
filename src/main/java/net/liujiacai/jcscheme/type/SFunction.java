ec0220e4cba60b42f410253f7a40ce398fc6b934
jiacai2050 <jiacai2050@gmail.com>
2015-10-11 20:33:35 +0800

    fix 'create new scope when apply a func'

--- src/main/java/net/liujiacai/jcscheme/type/SFunction.java
+++ src/main/java/net/liujiacai/jcscheme/type/SFunction.java
@@ -24,14 +24,17 @@ public class SFunction extends SObject {
 
 	public SObject apply(SObject... args) {
 		SScope originScope = SScope.current;
-		SScope.current = scope;
+		// create new scope based on scope when define this function,
+		// which accounts for JCScheme as a lexical-scoped language
+		SScope funcScope = new SScope(this.scope);
+		SScope.current = funcScope;
 		SObject ret = null;
 		for (int i = 0; i < args.length; i++) {
 			SScope.current.getEnv().put(param.get(i), args[i]);
 		}
 		if (args.length < param.size()) {
 			List<String> subParam = param.subList(args.length, param.size());
-			ret = new SFunction(subParam, body, new SScope(SScope.current));
+			ret = new SFunction(subParam, body, funcScope);
 		} else {
 			int bodySize = body.size();
 			for (int i = 0; i < bodySize - 1; i++) {
